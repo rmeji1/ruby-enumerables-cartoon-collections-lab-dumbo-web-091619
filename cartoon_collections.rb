7d96f203c2da26e0b915fddc7cfd8bd0e0f2a104
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 19:51:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,5 @@ def long_planeteer_calls(calls)# code an argument here
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
