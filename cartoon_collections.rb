9075bb2538f3e6c9dfea515831d3aa1f505724d9
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 19:43:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarfs)# code an argument here
-  dwarfs.each_with_index{ |value, index| puts "#{index}. #{value}"}
+  dwarfs.each_with_index{ |value, index| puts "#{index + 1}. #{value}"}
 end
 
 def summon_captain_planet# code an argument here
